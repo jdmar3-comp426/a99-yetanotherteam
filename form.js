1164205184474e7528cd7821be04b9b80813d5b1
Simmi Agarwal <simmi@live.unc.edu>
2021-12-04 15:07:40 -0500

    login functionality added, doesn't yet change pages

--- form.js
+++ form.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", function () {
+    console.log("signupForm")
     function sendData() {
         // get data from form element
         const sendRequest = new XMLHttpRequest();
@@ -23,14 +24,13 @@ window.addEventListener("load", function () {
     // define location of form to get data from
     const form = document.getElementById("signupForm");
 
-    var password = document.getElementById("pass")
-    var password_retype = document.getElementById("pass_retype");
+    // Password Validation
+    // var password = document.getElementById("pass")
+    // var password_retype = document.getElementById("pass_retype");
 
     // function validatePassword() {
     //     if (password.value != password_retype.value) {
     //         password_retype.setCustomValidity("Passwords Don't Match");
-    //     } else {
-    //         password_retype.setCustomValidity('');
     //     }
     // }
 
@@ -39,6 +39,38 @@ window.addEventListener("load", function () {
 
     // listener that calls send data function
     form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        sendData();
+    });
+});
+
+window.addEventListener("load", function () {
+    console.log("loginForm")
+    function sendData() {
+        // get data from form element
+        const sendRequest = new XMLHttpRequest();
+        const loginInfo = new FormData(login);
+
+        // error handling
+        sendRequest.addEventListener("error", function (event) {
+            alert("Submission unsuccessful, please check your input and try again");
+        });
+
+        // success message
+        sendRequest.addEventListener("load", function (event) {
+            alert("Successfully logged in");
+        });
+
+        // open endpoint and send data
+        sendRequest.open("GET", "http://localhost:5000/app/auth");
+        sendRequest.send(loginInfo);
+    }
+
+    // define location of form to get data from
+    const login = document.getElementById("loginForm");
+
+    // listener that calls send data function
+    login.addEventListener("submit", function (event) {
         event.preventDefault();
         sendData();
     });
