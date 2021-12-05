ed0d62f1a3b42847b4108e9e391246b3f99bd264
ose <oseizo@live.unc.edu>
2021-12-04 19:43:50 -0500

    added email

--- username.js
+++ username.js
@@ -21,8 +21,35 @@ $(document).ready(function () {
     passwordinput.select();
   });
 
-  $("#Save_Button").click(function () {
-    window.location.reload();
+  $("#Change_Email_Text").click(function () {
+    var passwordinput = $("<input>", {
+      val: $("#comp126_Text  span").text(),
+      type: "text",
+      id: "comp126_Text",
+    });
+    Save_Button.style.display = "inline-block";
+    $("#comp126_Text").replaceWith(passwordinput);
+    passwordinput.select();
+  });
 
+  $("#Save_Button").click(function () {
+    var userText = $("<span>", {
+      text: $("#abcd123_Text").val(),
+      id: "abcd123_Text",
+    });
+    $("#abcd123_Text").replaceWith(userText);
+
+    var passText = $("<span>", {
+      text: $("#password123_Text").val(),
+      id: "password123_Text",
+    });
+    $("#password123_Text").replaceWith(passText);
+
+    var emailText = $("<span>", {
+      text: $("#comp126_Text").val(),
+      id: "comp126_Text",
+    });
+    $("#comp126_Text").replaceWith(emailText);
+    Save_Button.style.display = "none";
   });
 });
